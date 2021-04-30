4fe133c6576bcd613fe5004dc3917c90cad4149d
Saidul Hassan <SaidulHassan@users.noreply.github.com>
2021-04-30 20:14:13 +0600

    Add files via upload

---
+++ batch_move_webm.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Config parameters
+SET groupsize=200
+:: initial counter, everytime counter is 1, we create new folder
+SET n=1
+:: folder counter
+SET nf=0
+
+FOR %%f IN (*.webm) DO (
+  :: if counter is 1, create new folder
+  IF !n!==1 (
+    SET /A nf+=1
+    MD folder!nf!
+  )
+
+  :: move file into folder
+  MOVE /Y "%%f" folder!nf!
+
+  :: reset counter if larger than group size
+  IF !n!==!groupsize! (
+    SET n=1
+  ) ELSE (
+    SET /A n+=1
+  )
+)
+
+ENDLOCAL
+
+PAUSE
